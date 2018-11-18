724a11be976af23a045012935aca4f9145f46a0f
cvzi <cuzi@openmail.cc>
2018-11-18 19:52:37 +0100

    imdb

--- Show_Rottentomatoes_meter.user.js
+++ Show_Rottentomatoes_meter.user.js
@@ -13,7 +13,7 @@
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @license     GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
-// @version     8
+// @version     9
 // @connect     www.rottentomatoes.com
 // @include     https://play.google.com/store/movies/details/*
 // @include     http://www.amazon.com/*
@@ -354,7 +354,7 @@ var sites = {
   },
   'imdb' : {
     host : ["imdb.com"],
-    condition : Always,
+    condition : () => !~document.location.pathname.indexOf("/mediaviewer") && !~document.location.pathname.indexOf("/mediaindex") && !~document.location.pathname.indexOf("/videoplayer"),
     products : [
     {
       condition : function() { 
